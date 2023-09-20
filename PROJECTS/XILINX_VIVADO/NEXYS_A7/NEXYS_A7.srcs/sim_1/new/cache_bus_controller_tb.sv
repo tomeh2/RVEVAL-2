b703f15cc5eeb25ba73e251a9efecf13f74a3795
Tomislav Harmina <tomislav.harmina@gmail.com>
2023-09-20 20:00:46 +0200

    Added write logic to bus controller

--- PROJECTS/XILINX_VIVADO/NEXYS_A7/NEXYS_A7.srcs/sim_1/new/cache_bus_controller_tb.sv
+++ PROJECTS/XILINX_VIVADO/NEXYS_A7/NEXYS_A7.srcs/sim_1/new/cache_bus_controller_tb.sv
@@ -7,8 +7,10 @@ module cache_bus_controller_tb(
     
     logic clk, reset;
     logic [31:0] bus_addr_write, bus_data_write, bus_addr_read, bus_data_read, fetch_address, writeback_address, fetched_cacheline_data;
+    logic [127:0] writeback_data;
     logic [23:0] fetched_cacheline_tag;
-    logic [1:0] fetch_size, writeback_size;
+    logic [1:0] fetch_data_size, writeback_data_size;
+    logic [2:0] fetch_size, writeback_size;
     logic bus_stbr, bus_ackw, bus_ackr, fetch_cacheable, fetch_en, writeback_cacheable, writeback_en, fetched_cacheline_valid;
     logic [3:0] bus_stbw;
     
@@ -20,29 +22,40 @@ module cache_bus_controller_tb(
         #(T/2);
     end
     
-    task t_putReadRequest(input logic [31:0] addr, input logic cacheable, input logic [1:0] size);
+    task t_putReadRequest(input logic [31:0] addr, input logic [1:0] data_size, input logic [2:0] size = 3'b100);
         fetch_address <= addr;
-        fetch_cacheable <= cacheable;
         fetch_size <= size;
+        fetch_data_size <= data_size;
         fetch_en <= 1;
         
         @(posedge clk);
         #100ps;
         
         fetch_address <= 0;
-        fetch_cacheable <= 0;
         fetch_size <= 0;
         fetch_en <= 0;
     endtask;
     
-    task t_putWriteRequest();
-    
+    task t_putWriteRequest(input logic [31:0] addr, input logic [127:0] data, input logic [1:0] data_size, input logic [2:0] size = 3'b100);
+        writeback_address <= addr;
+        writeback_data <= data;
+        writeback_size <= size;
+        writeback_data_size <= data_size;
+        writeback_en <= 1;
+        
+        @(posedge clk);
+        #100ps;
+        writeback_address <= 0;
+        writeback_data <= 0;
+        writeback_size <= 0;
+        writeback_data_size <= 0;
+        writeback_en <= 0;
     endtask;
     
     cache_bus_controller_2      #(.ADDRESS_WIDTH(32),
                                   .DATA_WIDTH(32),
-                                  .ENTRY_SIZE_BYTES(4),
-                                  .ENTRIES_PER_CACHELINE(4),
+                                  .BYTES_PER_ENTRY(4),
+                                  .MAX_BURST_LENGTH(4),
                                   .TAG_BITS(24),
                                   
                                   .READ_FIFO_DEPTH(4),
@@ -57,8 +70,8 @@ module cache_bus_controller_tb(
                                  .bus_ackr(bus_ackr),
                                  
                                  .fetch_address(fetch_address),
-                                 .fetch_cacheable(fetch_cacheable),
-                                 .fetch_size(fetch_size),
+                                 .fetch_burst_length(fetch_size),
+                                 .fetch_data_size(fetch_data_size),
                                  .fetch_en(fetch_en),
                                  
                                  .fetched_cacheline_data(fetched_cacheline_data),
@@ -66,8 +79,9 @@ module cache_bus_controller_tb(
                                  .fetched_cacheline_valid(fetched_cacheline_valid),
                                  
                                  .writeback_address(writeback_address),
-                                 .writeback_size(writeback_size),
-                                 .writeback_cacheable(writeback_cacheable),
+                                 .writeback_data(writeback_data),
+                                 .writeback_burst_length(writeback_size),
+                                 .writeback_data_size(writeback_data_size),
                                  .writeback_en(writeback_en),
                                  
                                  .clk(clk),
@@ -83,18 +97,50 @@ module cache_bus_controller_tb(
         bus_ackw <= 1;    
         bus_data_read <= 32'hF0F0_FFFF;
         
-        t_putReadRequest(32'h0000_0000, 1, 2'h00);
-        t_putReadRequest(32'h1000_0000, 1, 2'h00);
-        t_putReadRequest(32'h2000_0000, 1, 2'h00);
-        t_putReadRequest(32'h3000_0000, 1, 2'h00);
-        t_putReadRequest(32'h4000_0000, 1, 2'h00);
-        t_putReadRequest(32'h5000_0000, 1, 2'h00);
+        t_putReadRequest(32'h0000_0000, 2'h00);
+        t_putReadRequest(32'h1000_0000, 2'h00);
+        t_putReadRequest(32'h2000_0000, 2'h00);
+        t_putReadRequest(32'h3000_0000, 2'h00);
+        t_putReadRequest(32'h4000_0000, 2'h00);
+        t_putReadRequest(32'h5000_0000, 2'h00);
         #(T * 50);
         bus_data_read <= 32'hAAAA_AAAA;
-        t_putReadRequest(32'h0000_0010, 0, 2'h00);
-        t_putReadRequest(32'h1000_0020, 0, 2'h01);
-        t_putReadRequest(32'h2000_0030, 0, 2'h10);
-        t_putReadRequest(32'h3000_0040, 0, 2'h10);
+        t_putReadRequest(32'h0000_0010, 2'b00);
+        t_putReadRequest(32'h1000_0020, 2'b01);
+        t_putReadRequest(32'h2000_0030, 2'b10);
+        t_putReadRequest(32'h3000_0040, 2'b10);
+        #(T * 50);
+        reset <= 1;
+        #(T * 50);
+        reset <= 0;
+        t_putReadRequest(32'h0000_0010, 2'b10, 3'b100);
+        t_putReadRequest(32'h1000_0020, 2'b10, 3'b011);
+        t_putReadRequest(32'h2000_0030, 2'b10, 3'b010);
+        t_putReadRequest(32'h3000_0040, 2'b10, 3'b001);
+        t_putReadRequest(32'h3000_0050, 2'b10, 3'b000);
+        #(T * 50);
+        reset <= 1;
+        #(T * 50);
+        reset <= 0;
+        t_putWriteRequest(32'h0000_0000, 128'h0123_4567_89ab_cdef_0123_4567_89ab_cdef, 2'b10, 3'b100);
+        t_putWriteRequest(32'h1000_0000, 128'h0123_4567_89ab_cdef_0123_4567_89ab_cdef, 2'b10, 3'b100);
+        t_putWriteRequest(32'h2000_0000, 128'h0123_4567_89ab_cdef_0123_4567_89ab_cdef, 2'b10, 3'b100);
+        t_putWriteRequest(32'h3000_0000, 128'h0123_4567_89ab_cdef_0123_4567_89ab_cdef, 2'b10, 3'b100);
+        t_putWriteRequest(32'h4000_0000, 128'h0123_4567_89ab_cdef_0123_4567_89ab_cdef, 2'b10, 3'b100);
+        t_putWriteRequest(32'h5000_0000, 128'h0123_4567_89ab_cdef_0123_4567_89ab_cdef, 2'b10, 3'b100);
+        #(T * 50);
+        reset <= 1;
+        #(T * 50);
+        reset <= 0;
+        t_putWriteRequest(32'h3000_0030, 128'h0123_4567_89ab_cdef_0123_4567_89ab_cdef, 2'b10, 3'b001);
+        #(T * 20);
+        t_putWriteRequest(32'h4000_0040, 128'h0123_4567_89ab_cdef_0123_4567_89ab_cdef, 2'b01, 3'b001);
+        t_putWriteRequest(32'h4000_0042, 128'h0123_4567_89ab_cdef_0123_4567_89ab_cdef, 2'b01, 3'b001);
+        #(T * 20);
+        t_putWriteRequest(32'h5000_0050, 128'h0123_4567_89ab_cdef_0123_4567_89ab_cdef, 2'b00, 3'b001);
+        t_putWriteRequest(32'h5000_0051, 128'h0123_4567_89ab_cdef_0123_4567_89ab_cdef, 2'b00, 3'b001);
+        t_putWriteRequest(32'h5000_0052, 128'h0123_4567_89ab_cdef_0123_4567_89ab_cdef, 2'b00, 3'b001);
+        t_putWriteRequest(32'h5000_0053, 128'h0123_4567_89ab_cdef_0123_4567_89ab_cdef, 2'b00, 3'b001);
         
     end initial;
 endmodule
--- PROJECTS/XILINX_VIVADO/NEXYS_A7/NEXYS_A7.srcs/sources_1/new/cache_bus_controller_2.vhd
+++ PROJECTS/XILINX_VIVADO/NEXYS_A7/NEXYS_A7.srcs/sources_1/new/cache_bus_controller_2.vhd
@@ -22,6 +22,8 @@
 --SOFTWARE.
 --===============================================================================
 
+-- TODO: WRITE -> READ FORWARDING
+-- TODO: GENERALIZE WRITE LOGIC FOR DIFFERENT DATA & BURST SIZES
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
@@ -32,8 +34,8 @@ entity cache_bus_controller_2 is
     generic(
         ADDRESS_WIDTH : integer;
         DATA_WIDTH : integer;
-        ENTRY_SIZE_BYTES : integer;
-        ENTRIES_PER_CACHELINE : integer;
+        BYTES_PER_ENTRY : integer;
+        MAX_BURST_LENGTH : integer;
         TAG_BITS : integer;
     
         READ_FIFO_DEPTH : integer;
@@ -50,16 +52,17 @@ entity cache_bus_controller_2 is
         bus_ackr : in std_logic;
     
         fetch_address : in std_logic_vector(ADDRESS_WIDTH - 1 downto 0);
-        fetch_cacheable : in std_logic;
-        fetch_size : in std_logic_vector(1 downto 0);
+        fetch_data_size : in std_logic_vector(1 downto 0);
+        fetch_burst_length : in std_logic_vector(f_bits_needed(MAX_BURST_LENGTH) downto 0);
         fetch_en : in std_logic;
-        fetched_cacheline_data : out std_logic_vector(ENTRIES_PER_CACHELINE * ENTRY_SIZE_BYTES * 8 - 1 downto 0);
+        fetched_cacheline_data : out std_logic_vector(MAX_BURST_LENGTH * BYTES_PER_ENTRY * 8 - 1 downto 0);
         fetched_cacheline_tag : out std_logic_vector(TAG_BITS - 1 downto 0);
         fetched_cacheline_valid : out std_logic;
         
+        writeback_data : in std_logic_vector(BYTES_PER_ENTRY * MAX_BURST_LENGTH * 8 - 1 downto 0);
         writeback_address : in std_logic_vector(ADDRESS_WIDTH - 1 downto 0);
-        writeback_size : in std_logic_vector(1 downto 0);
-        writeback_cacheable : in std_logic;
+        writeback_data_size : in std_logic_vector(1 downto 0);
+        writeback_burst_length : in std_logic_vector(f_bits_needed(MAX_BURST_LENGTH) downto 0);
         writeback_en: in std_logic;
         
         clk : in std_logic;
@@ -70,16 +73,16 @@ end cache_bus_controller_2;
 architecture rtl of cache_bus_controller_2 is
     type read_fifo_entry_type is record
         address : std_logic_vector(ADDRESS_WIDTH - 1 downto 0);
-        size : std_logic_vector(1 downto 0);
-        cacheable : std_logic;
+        burst_length : std_logic_vector(f_bits_needed(MAX_BURST_LENGTH) downto 0);
+        data_size : std_logic_vector(1 downto 0);
         valid : std_logic;
     end record;
     
     type write_fifo_entry_type is record
         address : std_logic_vector(ADDRESS_WIDTH - 1 downto 0);
-        data : std_logic_vector(ENTRY_SIZE_BYTES * ENTRIES_PER_CACHELINE * 8 - 1 downto 0);
-        size : std_logic_vector(1 downto 0);
-        cacheable : std_logic;
+        data : std_logic_vector(BYTES_PER_ENTRY * MAX_BURST_LENGTH * 8 - 1 downto 0);
+        data_size : std_logic_vector(1 downto 0);
+        burst_length : std_logic_vector(f_bits_needed(MAX_BURST_LENGTH) downto 0);
         valid : std_logic;
     end record;
 
@@ -104,10 +107,25 @@ architecture rtl of cache_bus_controller_2 is
     signal read_state : read_sm_states_type;
     signal read_state_next : read_sm_states_type;
     signal read_fetch_addr_reg : std_logic_vector(ADDRESS_WIDTH - 1 downto 0);
-    signal read_fetch_data_reg : std_logic_vector(ENTRIES_PER_CACHELINE * ENTRY_SIZE_BYTES * 8 - 1 downto 0);
-    signal read_fetch_size_reg : std_logic_vector(1 downto 0);
-    signal read_fetched_words_counter : unsigned(f_bits_needed(ENTRIES_PER_CACHELINE) downto 0);
-    signal read_words_to_fetch : unsigned(f_bits_needed(ENTRIES_PER_CACHELINE) downto 0);
+    signal read_fetch_data_reg : std_logic_vector(MAX_BURST_LENGTH * BYTES_PER_ENTRY * 8 - 1 downto 0);
+    signal read_fetch_size_reg : std_logic_vector(f_bits_needed(MAX_BURST_LENGTH) downto 0);
+    signal read_fetched_words_counter : unsigned(f_bits_needed(MAX_BURST_LENGTH) downto 0);
+    signal read_burst_length : unsigned(f_bits_needed(MAX_BURST_LENGTH) downto 0);
+    
+    type write_sm_states_type is (
+        IDLE,
+        WRITE,
+        WRITE_DONE
+    );
+    signal write_state : write_sm_states_type;
+    signal write_state_next : write_sm_states_type;
+    signal write_addr_reg : std_logic_vector(ADDRESS_WIDTH - 1 downto 0);
+    signal write_data_reg : std_logic_vector(MAX_BURST_LENGTH * BYTES_PER_ENTRY * 8 - 1 downto 0);
+    signal write_size_reg : std_logic_vector(1 downto 0);
+    signal written_words_counter : unsigned(f_bits_needed(MAX_BURST_LENGTH) downto 0);
+    signal write_burst_length : unsigned(f_bits_needed(MAX_BURST_LENGTH) downto 0);
+    signal write_cycle_start : std_logic;
+    signal bus_stbw_temp : std_logic_vector(3 downto 0);
     
     signal write_fifo_head : unsigned(f_bits_needed(WRITE_FIFO_DEPTH) - 1 downto 0);
     signal write_fifo_head_next : unsigned(f_bits_needed(WRITE_FIFO_DEPTH) - 1 downto 0);
@@ -154,26 +172,43 @@ begin
                 -- ================ READ FIFO ================
                 if (read_fifo_enqueue = '1') then
                     read_fifo_tail <= read_fifo_tail_next;
-                    read_fifo_num_elements <= read_fifo_num_elements + 1;
                 end if;
                 
                 if (read_fifo_dequeue = '1') then
                     if (read_fifo_empty = '0') then
                         read_fifo_head <= read_fifo_head_next;
-                        read_fifo_num_elements <= read_fifo_num_elements - 1;
                     end if;
+                end if;
+                
+                if (read_fifo_enqueue = '1' and read_fifo_dequeue = '1') then
+                    read_fifo_num_elements <= read_fifo_num_elements;
+                elsif (read_fifo_enqueue = '1') then
+                    read_fifo_num_elements <= read_fifo_num_elements + 1;
+                elsif (read_fifo_dequeue = '1') then
+                    read_fifo_num_elements <= read_fifo_num_elements - 1;
                 end if;
                 -- ===========================================
                 
                 -- ================ WRITE FIFO ================
                 if (write_fifo_enqueue = '1') then
                     write_fifo_tail <= write_fifo_tail_next;
+                    
                 end if;
                 
                 if (write_fifo_dequeue = '1') then
                     if (write_fifo_empty = '0') then
                         write_fifo_head <= write_fifo_head_next;
+                        write_fifo_num_elements <= write_fifo_num_elements - 1;
                     end if;
+                end if;
+                
+                if (write_fifo_enqueue = '1' and write_fifo_dequeue = '1') then
+                    write_fifo_num_elements <= write_fifo_num_elements;
+                elsif (write_fifo_enqueue = '1') then
+                    write_fifo_num_elements <= write_fifo_num_elements + 1;
+                elsif (write_fifo_dequeue = '1') then
+                    write_fifo_num_elements <= write_fifo_num_elements - 1;
+                    
                 end if;
                 -- ============================================
             end if;
@@ -191,8 +226,8 @@ begin
             else
                 if (read_fifo_enqueue = '1') then
                     read_fifo(to_integer(read_fifo_tail)).address <= fetch_address;
-                    read_fifo(to_integer(read_fifo_tail)).size <= fetch_size;
-                    read_fifo(to_integer(read_fifo_tail)).cacheable <= fetch_cacheable;
+                    read_fifo(to_integer(read_fifo_tail)).data_size <= fetch_data_size;
+                    read_fifo(to_integer(read_fifo_tail)).burst_length <= fetch_burst_length;
                     read_fifo(to_integer(read_fifo_tail)).valid <= '1';
                 end if;
                 
@@ -202,8 +237,9 @@ begin
                 
                 if (write_fifo_enqueue = '1') then
                     write_fifo(to_integer(write_fifo_tail)).address <= writeback_address;
-                    write_fifo(to_integer(write_fifo_tail)).size <= writeback_size;
-                    write_fifo(to_integer(write_fifo_tail)).cacheable <= writeback_cacheable;
+                    write_fifo(to_integer(write_fifo_tail)).data <= writeback_data;
+                    write_fifo(to_integer(write_fifo_tail)).data_size <= writeback_data_size;
+                    write_fifo(to_integer(write_fifo_tail)).burst_length <= writeback_burst_length;
                     write_fifo(to_integer(write_fifo_tail)).valid <= '1';
                 end if;
                 
@@ -228,7 +264,7 @@ begin
             when FETCH => 
                 read_state_next <= FETCH;
                 
-                if (read_fetched_words_counter = read_words_to_fetch - 1 and bus_ackr = '1') then
+                if (read_fetched_words_counter = read_burst_length - 1 and bus_ackr = '1') then
                     read_state_next <= FETCH_DONE;
                 end if;
             when FETCH_DONE => 
@@ -269,18 +305,17 @@ begin
     -- ============================================================================
     --                                  READ LOGIC
     -- ============================================================================
-    read_reg_cntrl : process(clk)
+    read_cntrl : process(clk)
     begin
         if (rising_edge(clk)) then
             if (reset = '1') then
-                read_fetched_words_counter <= to_unsigned(0, f_bits_needed(ENTRIES_PER_CACHELINE) + 1);
+                read_fetched_words_counter <= to_unsigned(0, f_bits_needed(MAX_BURST_LENGTH) + 1);
             else
                 if (fetch_cycle_start = '1') then
-                    read_fetched_words_counter <= to_unsigned(0, f_bits_needed(ENTRIES_PER_CACHELINE) + 1);
-                    read_words_to_fetch <= to_unsigned(ENTRIES_PER_CACHELINE, f_bits_needed(ENTRIES_PER_CACHELINE) + 1) when read_fifo(to_integer(read_fifo_head)).cacheable = '1' else
-                                           to_unsigned(1, f_bits_needed(ENTRIES_PER_CACHELINE) + 1);
+                    read_fetched_words_counter <= to_unsigned(0, f_bits_needed(MAX_BURST_LENGTH) + 1);
+                    read_burst_length <= unsigned(read_fifo(to_integer(read_fifo_head)).burst_length);
                                            
-                    read_fetch_size_reg <= read_fifo(to_integer(read_fifo_head)).size;
+                    read_fetch_size_reg <= read_fifo(to_integer(read_fifo_head)).burst_length;
                     read_fetch_addr_reg <= read_fifo(to_integer(read_fifo_head)).address;
                 end if;
                 
@@ -288,7 +323,8 @@ begin
                     read_fetched_words_counter <= read_fetched_words_counter + 1;
                     read_fetch_addr_reg <= std_logic_vector(unsigned(read_fetch_addr_reg) + 4);
                     
-                    read_fetch_data_reg(ENTRY_SIZE_BYTES * 8 * to_integer(read_fetched_words_counter + 1) - 1 downto ENTRY_SIZE_BYTES * 8 * to_integer(read_fetched_words_counter)) <=
+                    
+                    read_fetch_data_reg(BYTES_PER_ENTRY * 8 * to_integer(read_fetched_words_counter + 1) - 1 downto BYTES_PER_ENTRY * 8 * to_integer(read_fetched_words_counter)) <=
                         bus_data_read;
                 end if;
             end if;
@@ -298,7 +334,125 @@ begin
     fetched_cacheline_tag <= read_fetch_addr_reg(31 downto 32 - TAG_BITS);
     
     bus_addr_read <= read_fetch_addr_reg;
-
+    
+    -- ============================================================================
+    --                            WRITE STATE MACHINE
+    -- ============================================================================
+    write_sm_next_state_reg_cntrl : process(all)
+    begin
+        case (write_state) is
+            when IDLE =>
+                write_state_next <= IDLE; 
+                if (write_fifo_empty = '0') then
+                    write_state_next <= WRITE;
+                end if;
+            when WRITE => 
+                write_state_next <= WRITE;
+                if (written_words_counter = write_burst_length - 1) then
+                    write_state_next <= WRITE_DONE;
+                end if;
+            when WRITE_DONE =>
+                write_state_next <= IDLE; 
+        end case;
+    end process;
+    
+    write_sm_state_reg_cntrl : process(clk)
+    begin
+        if (rising_edge(clk)) then
+            if (reset = '1') then
+                write_state <= IDLE;
+            else
+                write_state <= write_state_next;
+            end if;
+        end if;
+    end process;
+    
+    write_sm_eff : process(all)
+    begin
+        write_cycle_start <= '0';
+        write_fifo_dequeue <= '0';
+        bus_stbw <= (others => '0');
+        case (write_state) is
+            when IDLE => 
+                if (write_fifo_empty = '0') then
+                    write_cycle_start <= '1';
+                end if;
+            when WRITE => 
+                bus_stbw <= bus_stbw_temp;
+            when WRITE_DONE => 
+                write_fifo_dequeue <= '1';
+        end case;
+    end process;
+    
+    -- ============================================================================
+    --                               WRITE LOGIC
+    -- ============================================================================
+    write_cntrl : process(clk)
+    begin
+        if (rising_edge(clk)) then
+            if (reset = '1') then
+                
+            else
+                if (write_cycle_start = '1') then
+                    written_words_counter <= to_unsigned(0, f_bits_needed(MAX_BURST_LENGTH) + 1);
+                    write_burst_length <= unsigned(write_fifo(to_integer(write_fifo_head)).burst_length);
+                    write_addr_reg <= write_fifo(to_integer(write_fifo_head)).address;
+                    
+                    write_data_reg <= write_fifo(to_integer(write_fifo_head)).data;
+                    write_size_reg <= write_fifo(to_integer(write_fifo_head)).data_size;
+                end if;
+                
+                if (bus_ackw = '1' and write_state = WRITE) then
+                    written_words_counter <= written_words_counter + 1;
+                    write_addr_reg <= std_logic_vector(unsigned(write_addr_reg) + 4);
+                    
+                    -- Shift to the right after every write so that the lower 32 bits of the register contain the data that we want to write next
+                    write_data_reg(BYTES_PER_ENTRY * MAX_BURST_LENGTH * 8 - 1 downto BYTES_PER_ENTRY * MAX_BURST_LENGTH * 8 - 32) <= (others => '0');
+                    write_data_reg(BYTES_PER_ENTRY * MAX_BURST_LENGTH * 8 - 33 downto 0) <= write_data_reg(BYTES_PER_ENTRY * MAX_BURST_LENGTH * 8 - 1 downto 32);
+                end if;
+            end if;
+        end if;
+    end process;
+    bus_addr_write <= write_addr_reg;
+    
+    write_data_cntrl : process(all)
+    begin
+        bus_data_write <= (others => '0');
+        bus_stbw_temp <= (others => '0');
+        case write_size_reg is
+            when "00" =>                      -- BYTE (8 bits)
+                case write_addr_reg(1 downto 0) is
+                    when "00" =>
+                        bus_data_write(7 downto 0) <= write_data_reg(7 downto 0); 
+                        bus_stbw_temp <= "0001";
+                    when "01" =>
+                        bus_data_write(15 downto 8) <= write_data_reg(7 downto 0);
+                        bus_stbw_temp <= "0010"; 
+                    when "10" => 
+                        bus_data_write(23 downto 16) <= write_data_reg(7 downto 0);
+                        bus_stbw_temp <= "0100";
+                    when "11" => 
+                        bus_data_write(31 downto 24) <= write_data_reg(7 downto 0);
+                        bus_stbw_temp <= "1000";
+                    when others =>
+                end case;
+            when "01" =>                      -- HALF-WORD (16 bits)
+                case write_addr_reg(1) is
+                    when '0' =>
+                        bus_data_write(15 downto 0) <= write_data_reg(15 downto 0);
+                        bus_stbw_temp <= "0011"; 
+                    when '1' =>
+                        bus_data_write(31 downto 16) <= write_data_reg(15 downto 0);
+                        bus_stbw_temp <= "1100"; 
+                    when others =>
+                end case;
+            when "10" =>                      -- WORD (32 bits)
+                bus_data_write <= write_data_reg(31 downto 0);
+                bus_stbw_temp <= "1111";
+            when others =>
+                
+        end case;
+    end process;
 end rtl;
 
 
